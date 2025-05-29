19fbf8c0eac8e98b903731b3000a6ecf14209eac
Benjamin Nyakambangwe <benjaminnyakambangwe@gmail.com>
2025-05-29 09:31:38 +0200

    redirect to login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_login(request):
+    return redirect('users:login')
 
 urlpatterns = [
+    path('', redirect_to_login, name='home'),  # Redirect root URL to login
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
